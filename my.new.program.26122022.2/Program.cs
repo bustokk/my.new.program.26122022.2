476b489dd81a507d4a049c6d01cfbc0c51c2e57f
busto <busto@LAPTOP-82S7MAC0>
2022-12-26 23:54:29 +0300

    Add project files.

---
+++ my.new.program.26122022.2/Program.cs
@@ -0,0 +1,31 @@
+﻿  //Puan Ortalaması Hesaplama
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int vize1, vize2, final;
+
+        Console.Write("Vize Puanı : ");
+        vize1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Vize Puanı : ");
+        vize2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Final Puanı : ");
+        final = Convert.ToInt32(Console.ReadLine());
+
+        double ortalama = (vize1 * 0.2) + (vize2 * 0.3) + (final * 0.5);
+        Console.WriteLine("Ortalama Puanınız : " + " " + (double)ortalama);
+
+        if (ortalama >= 50)
+        {
+            Console.WriteLine("Geçtiniz.");
+        }
+        else if (ortalama >= 50)
+        {
+            Console.WriteLine("Kaldınız.");
+        }
+
+        Console.ReadKey();
+    }
+}
