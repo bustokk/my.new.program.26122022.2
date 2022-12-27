364bdb3a16cb0a4a7fb9d38cacc778337f314d4f
bustok <121151499+bustokk@users.noreply.github.com>
2022-12-27 11:15:00 +0300

    Update Program.cs

--- my.new.program.26122022.2/Program.cs
+++ my.new.program.26122022.2/Program.cs
@@ -1,31 +1,134 @@
-﻿  //Puan Ortalaması Hesaplama
+using System.Globalization;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        int vize1, vize2, final;
 
-        Console.Write("Vize Puanı : ");
-        vize1 = Convert.ToInt32(Console.ReadLine());
+        //// SINAV ORTALAMASI HESAPLA-GEÇTİ - GEÇMEDİ
+        //int vize1, vize2, final, ortalama;
 
-        Console.Write("Vize Puanı : ");
-        vize2 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Vize1 : ");
+        //vize1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Vize2 : ");
+        //vize2 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Final : ");
+        //final = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("--------------------------------");
 
-        Console.Write("Final Puanı : ");
-        final = Convert.ToInt32(Console.ReadLine());
+        //ortalama = Convert.ToInt32(vize1 * 0.2 + vize2 * 0.3 + final * 0.5);
+        //Console.WriteLine();
+        //Console.WriteLine("Ortalamanız = {0}", ortalama);
+        //Console.WriteLine();
+        //if (ortalama < 50 || final < 50)
+        //    Console.WriteLine("Kaldınız.");
 
-        double ortalama = (vize1 * 0.2) + (vize2 * 0.3) + (final * 0.5);
-        Console.WriteLine("Ortalama Puanınız : " + " " + (double)ortalama);
+        //else
+        //    Console.WriteLine("Geçtiniz.");
 
-        if (ortalama >= 50)
-        {
-            Console.WriteLine("Geçtiniz.");
-        }
-        else if (ortalama >= 50)
-        {
-            Console.WriteLine("Kaldınız.");
-        }
+        //Console.ReadKey();
 
-        Console.ReadKey();
+
+        ////ASAL SAYILARI EKRANA YAZDIR
+        //int sayac = 0;
+        //for (int sayi = 2; sayi <= 100; sayi++)
+        //{
+        //    int kontrol = 0;
+
+        //    for (int i = 2; i < sayi; i++)
+        //    {
+        //        if (sayi % i == 0)
+        //        {
+        //            kontrol++;
+        //            break;
+        //        }
+        //    }
+        //    if (kontrol == 0)
+        //    {
+        //        sayac++;
+        //        Console.WriteLine(sayi);
+        //    }
+        //}
+        //Console.WriteLine("============================");
+        //Console.WriteLine("{0} adet asal sayı var", sayac);
+        //Console.ReadKey();
+
+        //// BANKAMATİK PARA SAYIMI
+        //        int para = 0;
+        //Console.Write("Para tutarını giriniz : ");
+        //para = int.Parse(Console.ReadLine());
+        //int a200 = 0, a100 = 0, a50 = 0, a20 = 0, a10 = 0, a5 = 0;
+
+        //if (para >= 200)
+        //{
+        //    a200 = para / 200;
+        //    para = para % 200;
+        //}
+
+        //if (para >= 100)
+        //{
+        //    a100 = para / 100;
+        //    para = para % 100;
+        //}
+
+        //if (para >= 50)
+        //{
+        //    a50 = para / 50;
+        //    para = para % 50;
+        //}
+
+        //if (para >= 20)
+        //{
+        //    a20 = para % 20;
+        //    para = para % 20;
+        //}
+
+        //if (para >= 10)
+        //{
+        //    a10 = para % 10;
+        //    para = para % 10;
+        //}
+
+        //if (para >= 5)
+        //{
+        //    a5 = para % 5;
+        //    para = para % 5;
+        //}
+
+        //Console.WriteLine("200'lük Banknot : " + a200);
+        //Console.WriteLine("100'lük Banknot : " + a100);
+        //Console.WriteLine("50'lük Banknot : " + a50);
+        //Console.WriteLine("20'lük Banknot : " + a20);
+        //Console.WriteLine("10'lük Banknot : " + a10);
+        //Console.WriteLine("5'lük Banknot : " + a5);
+        //Console.ReadKey();
+
+        //// GİRİLEN SAYININ BASAMAKLARI TOPLAMINI HESAPLAMA
+        //    Console.Write("Bir sayı giriniz : ");
+        //int sayi;
+        //int sonuc = 0;
+        //sayi = Convert.ToInt32(Console.ReadLine());
+        //while (sayi > 0)
+        //{
+        //    sonuc += sayi % 10;
+        //    sayi /= 10;
+        //}
+        //Console.WriteLine("Girilen sayının rakamları toplamı : " + sonuc);
+        //Console.ReadKey();
+
+        ////ÇARPMA İŞLEMİ KULLANMADAN ÇARPMA İŞLEMİ YAPMAK
+        //int sayi1, sayi2, sonuc = 0;
+        //Console.Write("Birinci sayıyı giriniz : ");
+        //sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("İkinci sayıyı giriniz : ");
+        //sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = 1; i <= sayi2; i++)
+        //{
+        //    sonuc = sayi1 + sonuc;
+        //}
+        //Console.WriteLine("Çarpımın Sonucu :" + sonuc);
+        //Console.ReadKey();
     }
 }
